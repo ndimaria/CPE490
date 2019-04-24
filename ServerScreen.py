6e127d722a939845c8d65e47879643cc234ba171
Besnik Balaj <besnikbalaj@Besniks-MBP.home>
2019-04-23 23:18:56 -0400

    Add two codes that were used as screen sharing

---
+++ ServerScreen.py
@@ -0,0 +1,53 @@
+from socket import socket
+from threading import Thread
+from zlib import compress
+
+from mss import mss
+
+#Need to find way so this updates based on screen laters
+WIDTH = 1900
+HEIGHT = 1000
+
+
+def retreive_screenshot(conn):
+    with mss() as sct:
+        # The region to capture
+        rect = {'top': 0, 'left': 0, 'width': WIDTH, 'height': HEIGHT}
+
+        while 'recording':
+            # Capture the screen
+            img = sct.grab(rect)
+            # Tweak the compression level here (0-9)
+            pixels = compress(img.rgb, 6)
+
+            # Send the size of the pixels length
+            size = len(pixels)
+            size_len = (size.bit_length() + 7) // 8
+            conn.send(bytes([size_len]))
+
+            # Send the actual pixels length
+            size_bytes = size.to_bytes(size_len, 'big')
+            conn.send(size_bytes)
+
+            # Send pixels
+            conn.sendall(pixels)
+
+#Need to update this to change host and port connection
+def main(host='0.0.0.0', port=5000):
+    sock = socket()
+    sock.connect((host, port))
+    try:
+        sock.listen(5)
+        print('Server started.')
+
+        while 'connected':
+            conn, addr = sock.accept()
+            print('Client connected IP:', addr)
+            thread = Thread(target=retreive_screenshot, args=(conn,))
+            thread.start()
+    finally:
+        sock.close()
+
+
+if __name__ == '__main__':
+    main()
