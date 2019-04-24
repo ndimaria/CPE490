5e0a7b650abff4e391abaffd49dfd738f2d0609b
Besnik Balaj <besnikbalaj@Besniks-MBP.home>
2019-04-23 23:32:06 -0400

    Commented out unneeded components

--- pythoncontroller.py
+++ pythoncontroller.py
@@ -6,55 +6,42 @@ class Joystick:
     def __init__(self,refreshRate = 30):
         self.proc = subprocess.Popen(['xboxdrv','--no-uinput','--detach-kernel-driver'], stdout=subprocess.PIPE, bufsize=0)
         self.pipe = self.proc.stdout
-        #
-        self.connectStatus = False  #will be set to True once controller is detected and stays on
-        self.reading = '0' * 140    #initialize stick readings to all zeros
-        #
-        self.refreshTime = 0    #absolute time when next refresh (read results from xboxdrv stdout pipe) is to occur
-        self.refreshDelay = 1.0 / refreshRate   #joystick refresh is to be performed 30 times per sec by default
-        #
-        # Read responses from 'xboxdrv' for upto 2 seconds, looking for controller/receiver to respond
+        self.connectStatus = False
+        self.reading = '0' * 140
+        self.refreshTime = 0
+        self.refreshDelay = 1.0 / refreshRate
         found = False
         waitTime = time.time() + 2
         while waitTime > time.time() and not found:
             readable, writeable, exception = select.select([self.pipe],[],[],0)
             if readable:
                 response = self.pipe.readline()
-                # Hard fail if we see this, so force an error
                 if response[0:7] == b'No Xbox':
                     raise IOError('No Xbox controller/receiver found')
-                # Success if we see the following
                 if response[0:12].lower() == b'press ctrl-c':
                     found = True
-                # If we see 140 char line, we are seeing valid input
                 if len(response) == 140:
                     found = True
                     self.connectStatus = True
                     self.reading = response
-        # if the controller wasn't found, then halt
         if not found:
             self.close()
             raise IOError('Unable to detect Xbox controller/receiver - Run python as sudo')
 
     def refresh(self):
-        # Refresh the joystick readings based on regular defined freq
         if self.refreshTime < time.time():
-            self.refreshTime = time.time() + self.refreshDelay  #set next refresh time
-            # If there is text available to read from xboxdrv, then read it.
+            self.refreshTime = time.time() + self.refreshDelay
             readable, writeable, exception = select.select([self.pipe],[],[],0)
             if readable:
-                # Read every line that is availabe.  We only need to decode the last one.
                 while readable:
                     response = self.pipe.readline()
-                    # A zero length response means controller has been unplugged.
                     if len(response) == 0:
                         raise IOError('Xbox controller disconnected from USB')
                     readable, writeable, exception = select.select([self.pipe],[],[],0)
-                # Valid controller response will be 140 chars.
                 if len(response) == 140:
                     self.connectStatus = True
                     self.reading = response
-                else:  #Any other response means we have lost wireless or controller battery
+                else:
                     self.connectStatus = False
 
     def connected(self):
@@ -97,104 +84,87 @@ class Joystick:
                 return (raw - deadzone) / (32767.0 - deadzone)
 
     # Dpad Up status - returns 1 (pressed) or 0 (not pressed)
-    def dpadUp(self):
-        self.refresh()
-        return int(self.reading[45:46])
+###########DPAD##############
+#    def dpadUp(self):
+#        self.refresh()
+#        return int(self.reading[45:46])
 
-    # Dpad Down status - returns 1 (pressed) or 0 (not pressed)
-    def dpadDown(self):
-        self.refresh()
-        return int(self.reading[50:51])
+#    def dpadDown(self):
+#        self.refresh()
+#        return int(self.reading[50:51])
 
-    # Dpad Left status - returns 1 (pressed) or 0 (not pressed)
-    def dpadLeft(self):
-        self.refresh()
-        return int(self.reading[55:56])
+#    def dpadLeft(self):
+#        self.refresh()
+#        return int(self.reading[55:56])
 
-    # Dpad Right status - returns 1 (pressed) or 0 (not pressed)
-    def dpadRight(self):
-        self.refresh()
-        return int(self.reading[60:61])
+#    def dpadRight(self):
+#        self.refresh()
+#        return int(self.reading[60:61])
 
-    # Back button status - returns 1 (pressed) or 0 (not pressed)
-    def Back(self):
-        self.refresh()
-        return int(self.reading[68:69])
+#    def Back(self):
+#        self.refresh()
+#        return int(self.reading[68:69])
 
-    # Guide button status - returns 1 (pressed) or 0 (not pressed)
-    def Guide(self):
-        self.refresh()
-        return int(self.reading[76:77])
+#    def Guide(self):
+#        self.refresh()
+#        return int(self.reading[76:77])
 
-    # Start button status - returns 1 (pressed) or 0 (not pressed)
-    def Start(self):
-        self.refresh()
-        return int(self.reading[84:85])
+#    def Start(self):
+#        self.refresh()
+#        return int(self.reading[84:85])
 
-    # Left Thumbstick button status - returns 1 (pressed) or 0 (not pressed)
+########ThumbStick#################
     def leftThumbstick(self):
         self.refresh()
         return int(self.reading[90:91])
 
-    # Right Thumbstick button status - returns 1 (pressed) or 0 (not pressed)
     def rightThumbstick(self):
         self.refresh()
         return int(self.reading[95:96])
 
-    # A button status - returns 1 (pressed) or 0 (not pressed)
+##########Buttons################
     def A(self):
         self.refresh()
         return int(self.reading[100:101])
 
-    # B button status - returns 1 (pressed) or 0 (not pressed)
     def B(self):
         self.refresh()
         return int(self.reading[104:105])
 
-    # X button status - returns 1 (pressed) or 0 (not pressed)
     def X(self):
         self.refresh()
         return int(self.reading[108:109])
 
-    # Y button status - returns 1 (pressed) or 0 (not pressed)
     def Y(self):
         self.refresh()
         return int(self.reading[112:113])
-
-    # Left Bumper button status - returns 1 (pressed) or 0 (not pressed)
+        
+#########Bumpers#################
     def leftBumper(self):
         self.refresh()
         return int(self.reading[118:119])
 
-    # Right Bumper button status - returns 1 (pressed) or 0 (not pressed)
     def rightBumper(self):
         self.refresh()
         return int(self.reading[123:124])
 
-    # Left Trigger value scaled between 0.0 to 1.0
+##########Triggers###############
     def leftTrigger(self):
         self.refresh()
         return int(self.reading[129:132]) / 255.0
 
-    # Right trigger value scaled between 0.0 to 1.0
     def rightTrigger(self):
         self.refresh()
         return int(self.reading[136:139]) / 255.0
 
-    # Returns tuple containing X and Y axis values for Left stick scaled between -1.0 to 1.0
-    # Usage:
-    #     x,y = joy.leftStick()
+##########StickDeadZone############
     def leftStick(self,deadzone=4000):
         self.refresh()
         return (self.leftX(deadzone),self.leftY(deadzone))
 
-    # Returns tuple containing X and Y axis values for Right stick scaled between -1.0 to 1.0
-    # Usage:
-    #     x,y = joy.rightStick()
     def rightStick(self,deadzone=4000):
         self.refresh()
         return (self.rightX(deadzone),self.rightY(deadzone))
 
-    # Cleanup by ending the xboxdrv subprocess
     def close(self):
         self.proc.kill()
