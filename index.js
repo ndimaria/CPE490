f0cd91856e6fc9e03bdbbc59b338db56274f721e
Nathan <natvaneck@gmail.com>
2019-04-29 09:11:49 -0400

    User Identification 0.2
    Clients now send their userIDs to the server which keeps track of them and the associated socket connections.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+let users = {};
+
+let sockets = {};
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,8 +16,17 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    //console.log('message: ' + msg);
     io.emit('chat message', msg);
+  });
+  //More identifying information should be passed through this function
+  socket.on('uid', function(uid, socket){
+    console.log('Received uid: ', uid);
+    let newUser = {
+      userID: uid;
+    };
+    sockets[newUser.userID] = socket;
+    users[newUser.userID] = newUser;
   });
 });
 
