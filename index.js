72fea92f800ed8d651301909969979511c3f4691
Besnik Balaj <besnikbalaj@Besniks-MacBook-Pro.local>
2019-05-01 19:42:45 -0400

    Lmao

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ io.on('connection', function(socket){
     let newUser = {
       userID: uid
     };
+    console.log(color)
     users[newUser.userID] = newUser;
     io.emit('chat message', "User " + uid + " has joined the chat!");
   });
