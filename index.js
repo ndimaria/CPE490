d43a46d4282f97489f0eec0ffc70bdc4b5b2e22c
Nathan <natvaneck@gmail.com>
2019-05-01 16:47:26 -0400

    HTTPS Integration 0.4
    Gave server user permissions to access /etc/ and set script to pull cert from there

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var http = require('http').createServer(app);
 let users = {};
 
 const certOptions = {
-  key: fs.readFileSync('privkey.pem'),
-  cert: fs.readFileSync('fullchain.pem')
+  key: fs.readFileSync('/etc/letsencrypt/live/demo.roomsfh.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/demo.roomsfh.org/fullchain.pem')
 };
 
 let appl = express();
