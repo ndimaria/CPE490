667bdcb71d38d9508bdb59f792dab5d0444eb462
Nathan <natvaneck@gmail.com>
2019-05-02 16:15:55 -0400

    HTTP Forwarding 0.6
    Bugfixes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 var app;
 
 var httpServer = http.createServer(express);
-httpServer.get("*", function(req, res){
+http.get("*", function(req, res){
   res.redirect('https://' + req.headers.host + req.url);
 });
 
